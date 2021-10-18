c447a62287793ae0df880858c54db2fd112d4a39
mmdzov <mzov939@gmail.com>
2021-10-19 01:29:00 +0330

    Easy init and install typescript & install requirement tools

---
+++ src/iu.ts
@@ -0,0 +1,30 @@
+// @flow
+
+/*
+ * @Name: IU JS
+ * @Repository: https://github.com/IUJS/IU
+ * @License: GPLv3
+ * @Date: 18 Oct, 2021
+ *
+ */
+
+function square(n: number): number {
+  return n * n;
+}
+
+square(10);
+// square("2")
+
+function concat(a: string, b: string) {
+  return a + b;
+}
+
+
+concat("A", "B");
+// concat(1, 2)
+
+function test() {
+  // test comment //
+}
+
+var a = "b";
