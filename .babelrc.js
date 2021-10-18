9fae85f619ed5d90b07c3d935027d9ab279cb32f
Max Base <MaxBaseCode@Gmail.Com>
2021-10-18 22:29:33 +0330

    Create .babelrc.js

---
+++ .babelrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  presets: [
+    require('@babel/preset-env'),
+  ],
+  plugins: [
+    require('@babel/plugin-syntax-dynamic-import')
+  ],
+  ignore: [
+    'dist/*.js',
+    'packages/**/*.js'
+  ]
+}
