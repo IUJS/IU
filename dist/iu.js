3cb7258120e83682535b7dec9467a003b1d5e795
Max Base <MaxBaseCode@gmail.com>
2021-10-19 09:07:09 +0330

    Build the TS in dist directory

---
+++ dist/iu.js
@@ -0,0 +1,23 @@
+"use strict";
+// @flow
+/*
+ * @Name: IU JS
+ * @Repository: https://github.com/IUJS/IU
+ * @License: GPLv3
+ * @Date: 18 Oct, 2021
+ *
+ */
+function square(n) {
+    return n * n;
+}
+square(10);
+// square("2")
+function concat(a, b) {
+    return a + b;
+}
+concat("A", "B");
+// concat(1, 2)
+function test() {
+    // test comment //
+}
+var a = "b";
